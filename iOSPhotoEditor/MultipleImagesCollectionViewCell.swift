549c65ff8164dab95bebca2c5a415b0aacdd8d98
Navneet Chaudhary <navneet2641997@gmail.com>
2020-05-27 18:27:47 +0530

    rearranged files

---
+++ iOSPhotoEditor/MultipleImagesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MultipleImagesCollectionViewCell.swift
+//  iOSPhotoEditor
+//
+//  Created by Navneet Chaudhary on 08/05/20.
+//
+
+import UIKit
+
+class MultipleImagesCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
